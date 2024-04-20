703b2ab6f8ee2fdf50cba47f7e32dec12991a523
GERALD ADDEY <geraldaddey@yahoo.com>
2024-04-20 10:56:12 +0000

    feat: add layout

---
+++ src/components/IncomeExpense.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="inc-exp-container">
+        <div>
+            <h4>Income</h4>
+            <p id="money-plus" class="money plus">+GHS{{ income }}</p>
+        </div>
+        <div>
+            <h4>Expense</h4>
+            <p id="money-minus" class="money minus">-GHS{{ expenses }}</p>
+        </div>
+    </div>
+</template>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,13 @@
+<template>
+    <h3>History</h3>
+    <ul id="list" class="list">
+        <li class="minus">
+            Cash <span>GHS 400 </span>
+            <button class="delete-btn">x</button>
+        </li>
+        <li class="plus">
+            Credit <span>GHS 200 </span>
+            <button class="delete-btn">x</button>
+        </li>
+    </ul>
+</template>
