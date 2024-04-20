600dc34d56774959d4e9b7f2b4210dec22b2dd5e
GERALD ADDEY <geraldaddey@yahoo.com>
2024-04-20 11:50:19 +0000

    feat: add history, income and expense

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="container">
         <Header />
-        <Balance />
-        <IncomeExpense />
-        <TransactionList :transactions="transactions"/>
+        <Balance :total="total"/>
+        <IncomeExpense :income="income" :expense="expense"/>
+        <TransactionList :transactions="transactions" />
         <AddTransaction />
     </div>
 </template>
@@ -14,20 +14,55 @@ import Header from './components/Header.vue';
 import Balance from './components/Balance.vue';
 import IncomeExpense from './components/IncomeExpense.vue';
 import TransactionList from './components/TransactionList.vue';
-import AddTransaction from './components/AddTransaction.vue'; 
+import AddTransaction from './components/AddTransaction.vue';
 
 
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
-const transactions = ref([
+const transactions = ref( [
     { id: 1, text: 'Flower', amount: -20 },
     { id: 2, text: 'Salary', amount: 300 },
     { id: 3, text: 'Book', amount: -10 },
-    { id: 4, text: 'Camera', amount: 150 }
+    { id: 4, text: 'Camera', amount: 1900 },
+    { id: 5, text: 'Box', amount: -44 }
 ] )
+
+// console.log(transactions.value)
+
+
+// Total Balance
+const total = computed( () => {
+    return transactions.value.reduce( ( acc, transaction ) => {
+        return acc + transaction.amount;
+    }, 0 );
+} )
+
+//Income 
+const income = computed( () => {
+    return transactions.value
+        .filter((transaction) => transaction.amount > 0)
+        .reduce( ( acc, transaction ) => {
+        return acc + transaction.amount;
+    }, 0 )
+    .toFixed( 2 );
+
+} )
+
+//Expense 
+const expense = computed( () => {
+    return transactions.value
+        .filter( (transaction) => transaction.amount < 0 )
+        .reduce( ( acc, transaction ) => {
+            return acc + transaction.amount;
+        }, 0 )
+        .toFixed(2);
+} )
+
+
+console.log(total.value)
 </script>
 
-
+ 
 <!-- <script>
 import Header from './components/Header.vue';
 import Balance from './components/Balance.vue';
--- src/components/IncomeExpense.vue
+++ src/components/IncomeExpense.vue
@@ -2,11 +2,28 @@
     <div class="inc-exp-container">
         <div>
             <h4>Income</h4>
-            <p id="money-plus" class="money plus">+GHS{{ income }}</p>
+            <p id="money-plus" class="money plus">GHS{{income}}</p>
         </div>
         <div>
             <h4>Expense</h4>
-            <p id="money-minus" class="money minus">-GHS{{ expenses }}</p>
+            <p id="money-minus" class="money minus">GHS{{ Math.abs(expense) }}</p>
         </div>
     </div>
 </template>
+
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps( {
+    income: {
+        type: Number,
+        required: true
+    },
+    expense: {
+        type: Number,
+        required: true
+    }
+} )
+
+</script>
